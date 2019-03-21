a3999483f9825db51c43f448c914536d3404ba05
tomoakimiura <tomomiura0026@gmail.com>
2019-03-21 16:58:51 +0900

    omniauth_callbacks_controller.rbの編集:

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook; basic_action; end
+  def google; basic_action; end
+
+  private
+  def basic_action
+    @omniauth = request.env['omniauth.auth']
+    if @omniauth.present?
+      @profile = SocialProfile.where(provider: @omniauth['provider'], uid: @omniauth['uid']).first
+
+      if @profile
+        @profile.set_values(@omniauth)
+        sign_in_and_redirect @profile.user, event: :authentication
+      else
+          session[:nickname] = @omniauth['info']['name']
+          session[:email] = @omniauth['info']['email']
+          session[:password] = Devise.friendly_token[0, 20]
+          session[:provider] = @omniauth['provider']
+          session[:uid] = @omniauth['uid']
+          redirect_to users_new_path
+      end
+    end
+  end
+end
 
   # You should also create an action method in this controller like this:
   # def twitter
