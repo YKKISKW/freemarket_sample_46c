1e14455843624f244dcb0522e1d7b3e1dcff38a9
98hira <flattail2018@gmail.com>
2019-04-06 21:17:24 +0900

    💳indexアクションの呼び元を判定する関数を追加
    [time]2019/04/06-21:17

--- app/controllers/credit_card_controller.rb
+++ app/controllers/credit_card_controller.rb
@@ -3,6 +3,10 @@ class CreditCardController < ApplicationController
 
   # 支払い方法確認画面
   def index
+
+    # 呼び元に応じてビューの表示を変更する
+    @index_type = check_index_type()
+
     _credit_record = find_by_CreditRecord()
     if _credit_record && _credit_record.card_id
       @card_info = Credit.get_CardInfo(_credit_record)
@@ -54,6 +58,19 @@ class CreditCardController < ApplicationController
 
   def get_user_id()
     return current_user.id
+  end
+
+
+  def check_index_type()
+    _index_type = {
+      "orders#new"        => "order",  #購入画面の支払い方法変更から呼ばれた時
+      "credit_card#index" => "mypage", #マイページの支払い方法変更から呼ばれた時
+    }
+    # リンク元のコントローラ名とアクション名を取得
+    _s = Rails.application.routes.recognize_path(request.referer).slice(:controller, :action)
+    _source_action = "#{_s[:controller]}##{_s[:action]}"
+
+    return _index_type[_source_action]
   end
 
 end
